c4d97003b395c4559bf0986decfe1c00d4d02bbf
Viktar <rockcatx@gmail.com>
2021-07-10 23:40:44 +0300

    ДЗ в процессе

--- tasks/easy/decorators/repeat_n_times.py
+++ tasks/easy/decorators/repeat_n_times.py
@@ -3,3 +3,14 @@
 Написать декоратор repeat_n_times, у которого есть параметр n.
 Декоратор должен выполнить функцию bang n раз
 """
+def repeat_n_times(n: int):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(n):
+                func(*args, **kwargs)
+            return wrapper
+        return decorator
+
+    @repeat_n_times
+    def bang():
+        print("Boom")
--- tasks/easy/reqursion/factorial.py
+++ tasks/easy/reqursion/factorial.py
@@ -1,3 +1,11 @@
 """
 Написать рекурсивную функцию factorial, которая будет возвращать n-ый элемент
 """
+def factorial(n: int, curent=1, result: int = 1 ):
+    if current <= n:
+        factorial(n, curent + 1, result * current)
+    else:
+        return result
+
+
+    print(factorial(3))
